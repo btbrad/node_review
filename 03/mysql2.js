a197045f80084874723c537ba547e28136186863
bt <biantaobrad@gmail.com>
2020-05-18 01:45:30 +0800

    原生mysql

--- 03/mysql2.js
+++ 03/mysql2.js
@@ -10,11 +10,24 @@
   const connection = await mysql.createConnection(cfg)
   // console.log('connection', connection)
 
+  // 创建表
   let ret = await connection.execute(`
     CREATE TABLE IF NOT EXISTS test (
       id INT NOT NULL AUTO_INCREMENT,
       message VARCHAR(45) NULL,
       PRIMARY KEY (id))
   `)
-    console.log('ret', ret)
+    // console.log('ret', ret)
+
+    // 插入一条数据
+    // ret = await connection.execute(`
+    //   INSERT INTO test(message) VALUES(?)
+    // `, ['abc'])
+    // console.log(ret)
+
+    // 查询
+    const [rows, fields] = await connection.execute(`
+      SELECT * FROM test
+    `)
+    console.log(JSON.stringify(rows))
 })()
