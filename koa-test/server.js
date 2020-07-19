a62b53653a675605e9b30c8dd9bf7f65355f046a
btbrad <biantaobrad@gmail.com>
2020-07-19 22:40:58 +0800

    错误处理

--- koa-test/server.js
+++ koa-test/server.js
@@ -13,6 +13,18 @@ router.get('/a', async (ctx) => {
   ctx.body = 'aaa'
 })
 
+router.get('/login', async (ctx) => {
+  let { username, password } = ctx.query
+  // if (!username || !password) {
+  //   ctx.throw(400, 'neither username nor password can be empty!')
+  // } else {
+  //   ctx.body = '登录成功!'
+  // }
+  ctx.assert(username, 400, 'username can not be empty')
+  ctx.assert(password, 400, 'password can not be empty')
+  ctx.body = '成功'
+})
+
 app.listen(3000, () => {
   console.log('server running at http://localhost:3000')
 })
